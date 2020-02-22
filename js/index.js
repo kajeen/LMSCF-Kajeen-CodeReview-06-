0b9ceafd8134dee00448741aaeb08f1c748ab420
Kajeen Ahmed <kajeenahmed2@icloud.com>
2020-02-22 16:35:48 +0100

    lala

--- js/index.js
+++ js/index.js
@@ -12,60 +12,138 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var objectAustria;
+var objRestaurants;
+var objEvents;
+objRestaurants = [];
+objEvents = [];
 objectAustria = [];
-var iArray = [];
-var Name = ["BioFrische", "ON Restaurant", "St. Charles Church", "Schönbrunn Park, Vienna"];
+// let iArray = [];
+// let aArray = [];
+var Name = ["ON Restaurant", "Sahara", "BEETHOVEN | AN der Wien | DENKEN", "Monet bis Picasso"];
 var city = ["wien", "wien", "wien", "wien"];
-var bezirk = [15, 5, 1, 13];
-var ZIPCode = [1150, 1050, 1010, 1130];
-var img = ["../img/bio.jpg", "../img/on.jpg", "../img/ch.jpg", "../img/sch.jpg"];
-var address = ["Stutterheimstraße 6", "Wehrgasse 8", "Karlsplatz 1", "Maxingstraße 13b"];
-var telephoneNumber = ["019529215", "015854900"];
-var type = ["Indian", "Chinese"];
-var webAddress = ["biofrische.wien", "http://www.restaurant-on.at/"];
-var austria = /** @class */ (function () {
-    function austria(Name, city, address, ZIPCode, bezirk, img) {
+var bezirk = [5, 1, 6, 1];
+var ZIPCode = [1050, 1010, 1060, 1010];
+var img = ["img/on.jpg", "img/re3.jpg", "img/event2.jpg", "img/event1.jpg"];
+var address = ["Wehrgasse 8", "Babenbergerstraße 5", "Linke Wienzeile 6", "Albertinaplatz 1"];
+var telephoneNumber = ["015854900", "0676 3743206"];
+var type = ["Chinese", "Indian"];
+var webAddress = ["http://www.restaurant-on.at/", "https://www.delinski.at", "https://www.wienersymphoniker.at", "https://events.wien.info/"];
+var date = ["22. Feb 2020", "23. Feb 2020"];
+var time = ["10:00 - 18:00", "18:00 bis 20:30 "];
+var price = ["55", "Kostenlos"];
+var land = /** @class */ (function () {
+    function land(Name, city, address, ZIPCode, bezirk, img, webAddress) {
         this.city = city;
         this.address = address;
         this.ZIPCode = ZIPCode;
         this.bezirk = bezirk;
         this.img = img;
         this.Name = Name;
+        this.webAddress = webAddress;
     }
-    return austria;
+    return land;
 }());
-function creationinfo() {
-    for (var i = 0; i < 5; i++) {
-        var profil = new austria(Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i]);
-        objectAustria.push(profil);
-    }
-}
+// function creationinfo(){
+//     for (let i = 0; i<8; i++){
+//         let place = new land( Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i])
+//         objectAustria.push(place);
+//     }
+// }
 var restaurant = /** @class */ (function (_super) {
     __extends(restaurant, _super);
-    function restaurant(city, address, ZIPCode, bezirk, img, Name, telephoneNumber, type, webAddress) {
-        var _this = _super.call(this, city, address, ZIPCode, bezirk, img, Name) || this;
+    // desc: any;
+    function restaurant(city, address, ZIPCode, bezirk, img, Name, telephoneNumber, type) {
+        var _this = _super.call(this, city, address, ZIPCode, bezirk, img, Name, webAddress) || this;
         _this.telephoneNumber = telephoneNumber;
         _this.type = type;
-        _this.webAddress = webAddress;
         return _this;
+        // this.desc = desc;
     }
     return restaurant;
-}(austria));
+}(land));
+var events = /** @class */ (function (_super) {
+    __extends(events, _super);
+    function events(city, address, ZIPCode, bezirk, img, Name, date, time, price) {
+        var _this = _super.call(this, city, address, ZIPCode, bezirk, img, Name, webAddress) || this;
+        _this.date = date;
+        _this.time = time;
+        _this.price = price;
+        return _this;
+    }
+    return events;
+}(land));
+function creationRestaurant(type) {
+    if (type === 'land') {
+        for (var i = 0; i < 4; i++) {
+            var place = new land(Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i], webAddress);
+            objectAustria.push(place);
+        }
+    }
+    else if (type === 'restaurant') {
+        for (var i = 0; i < 4; i++) {
+            var res = new restaurant(Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i], telephoneNumber[i], type[i]);
+            objRestaurants.push(res);
+        }
+    }
+    // else if (type === 'events') {
+    //     for (let i = 0; i<2; i++){
+    //         let Eve = new events(Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i], date[i], time[i], price[i])
+    //         objRestaurants.push(Eve);
+    //     }
+    // }
+}
+// function showInfo(index) {
+// $(document).ready(function() {
+//         $(`#button${index}`).parent().find(".hideInfo").toggle();
+//     });
+// }
+function showMore(index) {
+    if ($("#res" + index).css('visibility') == 'hidden') {
+        $("#res" + index).css('visibility', 'visible');
+    }
+    else {
+        $("#res" + index).css('visibility', 'hidden');
+    }
+    showMore(index);
+    console.log(showMore(index));
+}
 function createCard() {
-    var a = "";
+    var basePage = "";
     for (var i = 0; i < objectAustria.length; i++) {
-        iArray.push(i);
-        a += "\n    <div id=\"upperCard" + i + "\" class=\"card mb-3\">\n    \t<div class=\"card-body\">\n            <h5 class=\"card-title\">" + objectAustria[i].Name + "</h5>\n            <p class=\"card-text\">( " + objectAustria[i].city + " ), " + objectAustria[i].bezirk + ".Bezirk</p>\n        </div>\n        <div class=\"py-5\">\n\t\t    <div class=\"row\">\n      \t\t\t<div class=\"col-lg-6 mb-3 mb-lg-0\">\n        \t\t\t<div class=\"hover hover-2 text-white rounded\"><img src=\"" + objectAustria[i].img + "\" alt=\"\">\n          \t\t\t\t<div class=\"hover-overlay\"></div>\n          \t\t\t\t<div class=\"hover-2-content px-5 py-4\">\n\t            \t\t\t<h3 class=\"hover-2-title text-uppercase font-weight-bold mb-0\"> <span class=\"font-weight-light\">Image </span>Caption</h3>\n\t           \t\t\t\t<p class=\"hover-2-description text-uppercase mb-0\">Lorem ipsum dolor sit amet, consectetur <br>adipisicing elit.</p>\n          \t\t\t\t</div>\n        \t\t\t</div>\n      \t\t\t</div>\n \t\t\t</div>\n\t\t</div>\n     </div>\n  ";
+        basePage += "\n    <div id=\"HPage\" class=\"card mb-3 col-lg-3 col-md-6 col-sm-12\">\n    \t<div class=\"card-body\">\n            <h5 class=\"card-title\">" + objectAustria[i].Name + "</h5>\n            <p class=\"card-text\">" + objectAustria[i].city + ", " + objectAustria[i].bezirk + ".Bezirk</p>\n        </div>\n        <div class=\"py-5\">\n\t\t    <div class=\"row\">\n      \t\t\t<div class=\"pl-2 pr-2 mb-3 mb-lg-0 foto1\">\n        \t\t\t<div class=\" mb-2 hover hover-2 text-white rounded lala\">\n        \t\t\t\t<img id=\"foto\" src=\"" + objectAustria[i].img + "\" class=\"card-img-top img-fluid\" alt=\"\">\n          \t\t\t\t<div class=\"hover-overlay\"></div>\n          \t\t\t\t<div class=\"hover-2-content px-5 py-4\">\n\t           \t\t\t\t<p class=\"hover-2-description text-uppercase mb-0\">Lorem ipsum dolor sit amet, consectetur <br>adipisicing elit.\n\t           \t\t\t\t</p>\n\t           \t\t\t</div>\n          \t\t\t</div>\n                    <button onclick=\"showMore(" + i + ")\" type=\"submit\" class=\"p-3\" id=\"button" + i + "\">more Infoes\n                    </button>\n      \t\t\t</div>\n \t\t\t</div>\n\t\t</div>\n    </div>\n \t";
     }
     ;
-    document.getElementById('cardsDiv').innerHTML = a;
+    document.getElementById('cardsDiv').innerHTML = basePage;
 }
-creationinfo();
-createCard();
-var place = /** @class */ (function (_super) {
-    __extends(place, _super);
-    function place(city, address, ZIPCode, bezirk, img, Name) {
-        return _super.call(this, city, address, ZIPCode, bezirk, img, Name) || this;
+;
+function createRestaurant() {
+    var CardsRestaurant = "";
+    for (var i = 0; i < objRestaurants.length; i++) {
+        CardsRestaurant += "\n             <div id=\"res" + i + "\" class=\"hiddenRes m-2 flex-shrink-1  card p-0 col-lg-3 col-md-6 col-sm-12 border\">\n                <div  class=\"Card\">\n                  \t<p>Address: " + objectAustria[i].address + "</p>\n                    <span>ZIP: " + objectAustria[i].ZIPCode + "</span>\n                    <p>Telephone number " + objRestaurants[i].telephoneNumber + "</p>\n                    <p>Web Adress: " + objRestaurants[i].webAddress + "</p>\n                    <p>Typ: " + objRestaurants[i].type + "</p>\n                </div>\n            </div>\n          \t";
     }
-    return place;
-}(austria));
+    ;
+    document.getElementById('Restaurants').innerHTML = CardsRestaurant;
+}
+// function createEvent(){
+//     var CardsEvent = "";
+//     for (let i=0; i < objEvents.length; i++) {
+//             CardsEvent += `
+//              <div id="res${i}" class="hiddenRes m-2 flex-shrink-1  card p-0 col-lg-3 col-md-6 col-sm-12 border">
+//                 <div  class="Card">
+//                 	<p>Address: ${objectAustria[i].address}</p>
+//                  <span>ZIP: ${objectAustria[i].ZIPCode}</span>
+//                 	<p>Date: ${objEvents[i].date}</p>
+//                 	<p>Time: ${objEvents[i].time}</p>
+//                    	<p>Price: ${objEvents[i].price}</p>
+//                 </div>
+//             </div>
+//                 `;
+//             };
+// 	document.getElementById('Events').innerHTML = CardsEvent;
+// }
+creationRestaurant('land');
+creationRestaurant('restaurants');
+createCard();
+createRestaurant();
+// createEvent();
+console.log(createRestaurant());
--- js/index.ts
+++ js/index.ts
@@ -1,100 +1,206 @@
 
-let objectAustria: Array<austria>;
+let objectAustria: Array<land>;
+let objRestaurants: Array<restaurant>;
+let objEvents: Array<events>;
+objRestaurants = [];
+objEvents = [];
 objectAustria = [];
-let iArray = [];
+// let iArray = [];
+// let aArray = [];
 
 
-let Name: Array<string>= ["BioFrische","ON Restaurant", "St. Charles Church", "Schönbrunn Park, Vienna"];
+let Name: Array<string>= ["ON Restaurant", "Sahara","BEETHOVEN | AN der Wien | DENKEN", "Monet bis Picasso"];
 let city: Array<string>= ["wien", "wien", "wien", "wien"];
-let bezirk: Array<number>= [15, 5, 1, 13];
-let ZIPCode: Array<number>= [1150, 1050, 1010, 1130];
-let img: Array<string>= ["../img/bio.jpg", "../img/on.jpg", "../img/ch.jpg", "../img/sch.jpg"];
-let address: Array<string>= ["Stutterheimstraße 6", "Wehrgasse 8", "Karlsplatz 1", "Maxingstraße 13b"];
-let telephoneNumber: Array<string>= ["019529215", "015854900"];
-let type: Array<string>= ["Indian", "Chinese"];
-let webAddress: Array<string>= ["biofrische.wien", "http://www.restaurant-on.at/"];
+let bezirk: Array<number>= [5, 1, 6, 1];
+let ZIPCode: Array<number>= [1050, 1010, 1060, 1010];
+let img: Array<string>= ["img/on.jpg", "img/re3.jpg", "img/event2.jpg", "img/event1.jpg"];
+let address: Array<string>= ["Wehrgasse 8", "Babenbergerstraße 5", "Linke Wienzeile 6", "Albertinaplatz 1"];
+let telephoneNumber: Array<string>= ["015854900", "0676 3743206"];
+let type: Array<string>= ["Chinese", "Indian"];
+let webAddress: Array<string>= ["http://www.restaurant-on.at/", "https://www.delinski.at", "https://www.wienersymphoniker.at", "https://events.wien.info/"];
+let date: Array<string>=["22. Feb 2020", "23. Feb 2020"];
+let time: Array<string>=["10:00 - 18:00", "18:00 bis 20:30 "];
+let price: Array<string>= ["55", "Kostenlos"];
 
 
-class austria {
+class land {
 	Name: string;
     city: string;
     bezirk: number;
     ZIPCode: number;
     img: string;
     address : string;
+    webAddress: string;
     
-    constructor(Name, city, address, ZIPCode,bezirk, img) {
+    constructor(Name, city, address, ZIPCode,bezirk, img, webAddress) {
         this.city = city;
         this.address = address;
         this.ZIPCode = ZIPCode;
         this.bezirk = bezirk;
         this.img = img;
         this.Name = Name;
+        this.webAddress = webAddress;
     }
 }
 
-function creationinfo(){
-    for (let i = 0; i<5; i++){
-        let profil = new austria( Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i])
-        objectAustria.push(profil);
-    }
-}
+// function creationinfo(){
+//     for (let i = 0; i<8; i++){
+//         let place = new land( Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i])
+//         objectAustria.push(place);
+//     }
+// }
 
 
 
-class restaurant extends austria{
+class restaurant extends land{
 	telephoneNumber: number;
 	type: string;
-	webAddress: string;
+	// desc: any;
 
-	constructor(city, address, ZIPCode, bezirk, img, Name, telephoneNumber, type, webAddress) {
-       super(city, address, ZIPCode, bezirk, img, Name); 
+	constructor(city, address, ZIPCode, bezirk, img, Name, telephoneNumber, type) {
+       super(city, address, ZIPCode, bezirk, img, Name, webAddress); 
        this.telephoneNumber = telephoneNumber;
        this.type = type;
-       this.webAddress = webAddress;
+       
+       // this.desc = desc;
 	}
 }
 
+
+class events extends land{
+	date: string;
+	time:string;
+	price:string;
+
+	constructor(city, address, ZIPCode, bezirk, img, Name, date, time, price) {
+       super(city, address, ZIPCode, bezirk, img, Name, webAddress); 
+       this.date = date;
+       this.time = time;
+       this.price = price;
+
+	}
+}
+
+
+function creationRestaurant(type){
+    if (type === 'land') {
+        for (let i = 0; i<4; i++){
+            let place = new land( Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i], webAddress)
+            objectAustria.push(place);
+        }
+    }
+    else if (type === 'restaurant') {
+        for (let i = 0; i<4; i++){
+            let res = new restaurant( Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i], telephoneNumber[i], type[i])
+            objRestaurants.push(res);
+        }
+    }
+    // else if (type === 'events') {
+    //     for (let i = 0; i<2; i++){
+    //         let Eve = new events(Name[i], city[i], address[i], ZIPCode[i], bezirk[i], img[i], date[i], time[i], price[i])
+    //         objRestaurants.push(Eve);
+    //     }
+    // }
+}
+
+
+
+// function showInfo(index) {
+// $(document).ready(function() {
+//         $(`#button${index}`).parent().find(".hideInfo").toggle();
+//     });
+// }
+
+
+function showMore(index) {
+    if($(`#res${index}`).css('visibility') == 'hidden') {
+        $(`#res${index}`).css('visibility', 'visible')
+    }
+    else {
+        $(`#res${index}`).css('visibility', 'hidden')
+    }
+showMore(index);
+console.log(showMore(index));
+}
 function createCard(){
-    var a = "";
+    var basePage = "";
     for (let i=0; i < objectAustria.length; i++) {
-        iArray.push(i);
-        a += `
-    <div id="upperCard${i}" class="card mb-3">
+
+        basePage += `
+    <div id="HPage" class="card mb-3 col-lg-3 col-md-6 col-sm-12">
     	<div class="card-body">
             <h5 class="card-title">${objectAustria[i].Name}</h5>
-            <p class="card-text">( ${objectAustria[i].city} ), ${objectAustria[i].bezirk}.Bezirk</p>
+            <p class="card-text">${objectAustria[i].city}, ${objectAustria[i].bezirk}.Bezirk</p>
         </div>
         <div class="py-5">
 		    <div class="row">
-      			<div class="col-lg-6 mb-3 mb-lg-0">
-        			<div class="hover hover-2 text-white rounded"><img src="${objectAustria[i].img}" alt="">
+      			<div class="pl-2 pr-2 mb-3 mb-lg-0 foto1">
+        			<div class=" mb-2 hover hover-2 text-white rounded lala">
+        				<img id="foto" src="${objectAustria[i].img}" class="card-img-top img-fluid" alt="">
           				<div class="hover-overlay"></div>
           				<div class="hover-2-content px-5 py-4">
-	            			<h3 class="hover-2-title text-uppercase font-weight-bold mb-0"> <span class="font-weight-light">Image </span>Caption</h3>
-	           				<p class="hover-2-description text-uppercase mb-0">Lorem ipsum dolor sit amet, consectetur <br>adipisicing elit.</p>
-          				</div>
-        			</div>
+	           				<p class="hover-2-description text-uppercase mb-0">Lorem ipsum dolor sit amet, consectetur <br>adipisicing elit.
+	           				</p>
+	           			</div>
+          			</div>
+                    <button onclick="showMore(${i})" type="submit" class="p-3" id="button${i}">more Infoes
+                    </button>
       			</div>
  			</div>
 		</div>
-     </div>
-  `	;
-	};
-	document.getElementById('cardsDiv').innerHTML = a;
+    </div>
+ 	`;
+};
+document.getElementById('cardsDiv').innerHTML = basePage;
+};
+	function createRestaurant(){
+    var CardsRestaurant = "";
+    for (let i=0; i < objRestaurants.length; i++) {
+            CardsRestaurant += `
+             <div id="res${i}" class="hiddenRes m-2 flex-shrink-1  card p-0 col-lg-3 col-md-6 col-sm-12 border">
+                <div  class="Card">
+                  	<p>Address: ${objectAustria[i].address}</p>
+                    <span>ZIP: ${objectAustria[i].ZIPCode}</span>
+                    <p>Telephone number ${objRestaurants[i].telephoneNumber}</p>
+                    <p>Web Adress: ${objRestaurants[i].webAddress}</p>
+                    <p>Typ: ${objRestaurants[i].type}</p>
+                </div>
+            </div>
+          	`;
+        };
+	document.getElementById('Restaurants').innerHTML = CardsRestaurant;
+	
 }
 
-creationinfo();
+
+// function createEvent(){
+//     var CardsEvent = "";
+//     for (let i=0; i < objEvents.length; i++) {
+//             CardsEvent += `
+//              <div id="res${i}" class="hiddenRes m-2 flex-shrink-1  card p-0 col-lg-3 col-md-6 col-sm-12 border">
+//                 <div  class="Card">
+//                 	<p>Address: ${objectAustria[i].address}</p>
+//                  <span>ZIP: ${objectAustria[i].ZIPCode}</span>
+//                 	<p>Date: ${objEvents[i].date}</p>
+//                 	<p>Time: ${objEvents[i].time}</p>
+//                    	<p>Price: ${objEvents[i].price}</p>
+//                 </div>
+//             </div>
+//                 `;
+//             };
+// 	document.getElementById('Events').innerHTML = CardsEvent;
+	
+// }
+
+
+
+creationRestaurant('land');
+creationRestaurant('restaurants');
 createCard();
-
-
-class place extends austria{
-
-	constructor(city, address, ZIPCode, bezirk, img, Name) {
-       super(city, address, ZIPCode, bezirk, img, Name); 
-	}
-}
+createRestaurant();
+// createEvent();
 
 
 
 
+console.log(createRestaurant())
